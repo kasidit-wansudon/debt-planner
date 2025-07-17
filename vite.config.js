cccca371b09a2c984d26fbfd5630c8b81c209d16
kasidit wansudon <kasidit.wans@gmail.com>
2025-07-18 01:05:14 +0700

    fix: Update API proxy URL to use production endpoint

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://debt-planner.onrender.com',
+      // '/api': 'http://localhost:3000',
     },
   },
 })
